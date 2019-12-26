0897b47d9171417a4f7b4bce82544b0d89f7030c
Eick <oe52920@gmail.com>
2019-12-26 15:24:16 +0800

    fix: 增加密码验证和一些安装提醒

--- scripts/registry_install.sh
+++ scripts/registry_install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
+echo "######################################################################"
+echo "Attention!!!!"
+echo "This is an clean installation, will delete all old data and container!"
+echo "Make sure you have old-data backup OR press ctrl+c to abort!"
+echo "Installation will start 10 seconds later..."
+echo "######################################################################"
+sleep 20
 #remove installation if exits
 if [[ `docker ps | grep registry | wc -l` > 0 ]]; 
 then
@@ -13,19 +20,24 @@ echo "##########################################"
 echo "Input your prefer registry port, like 5000"
 echo "##########################################"
 read -p "input:" port
-echo "#####################################################################"
-echo "Input your prefer registry storage location, like /home/registry-data"
-echo "#####################################################################"
+echo "################################################################"
+echo "Input your prefer registry storage location, like /home/registry"
+echo "################################################################"
 read -p "input:" location
-
+rm -rf $location
+mkdir -p ${location}/{auth,data}
+echo "###################################"
+echo "Input your prefer registry username"
+echo "###################################"
+read -p "input:" username
+echo "###################################"
+echo "Input your prefer registry password"
+echo "###################################"
+read -p "input:" password
+docker run --rm --entrypoint htpasswd registry -Bbn $username $password > ${location}/auth/htpasswd
+echo "Password Generated!"
 #start container
-docker run -d \
-        -e STORAGE_PATH=/registry \
-        -p ${port}:5000 \
-        -v ${location}:/var/lib/registry \
-        --restart=always \
-        --name docker-registry \
-        registry:latest
+docker run -d -v ${location}/auth:/auth -e "REGISTRY_AUTH=htpasswd" -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" -e "REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd" -p ${port}:5000 -v ${location}/data:/var/lib/registry --restart=always  --name docker-registry  registry
 
 # add insecure-registry
 sudo mkdir -p /etc/docker
@@ -44,18 +56,26 @@ echo "----------------------"
 docker ps -a
 echo "----------------------"
 
-echo "################################################"
+echo "##################################################"
 echo "Install Registry Success! URL: ${ipaddr}:${port}"
-echo "################################################"
+echo "##################################################"
 echo "start to test the registry, push a busybox"
 echo "##########################################"
 docker pull busybox
 docker tag busybox:latest ${ipaddr}:${port}/busybox:latest
+docker login ${ipaddr}:${port} -u $username -p $password
+echo "##########"
+echo "Logged in!"
+echo "##########"
 docker push ${ipaddr}:${port}/busybox:latest
-echo "#########################################"
-echo "curl http://${ipaddr}:${port}/v2/_catalog"
-echo "#########################################"
-curl http://${ipaddr}:${port}/v2/_catalog
+echo "####################################################################"
+echo "curl --basic -u username:passwd http://${ipaddr}:${port}/v2/_catalog"
+echo "####################################################################"
+curl --basic -u ${username}:${password} http://${ipaddr}:${port}/v2/_catalog
 echo "#############"
 echo "Push Success!"
 echo "#############"
+docker logout ${ipaddr}:${port}
+echo "###########"
+echo "Logged Out!"
+echo "###########"
