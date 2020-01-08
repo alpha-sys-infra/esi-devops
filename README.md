beac2767a7f40bb821a7ee270e1ce16a49085b80
Eick <oe52920@gmail.com>
2020-01-08 17:24:44 +0800

    feat: redis scale模板

--- README.md
+++ README.md
@@ -12,7 +12,9 @@
 ----| docker_install.sh __ docker安装脚本
 ----| registry_install.sh __ docker registry安装脚本
 ----| registry_ui_install.sh __ registry ui安装脚本
+----| template-scale.sh __ redis scale模板
 ----| template.sh __ nodejs微服务生成容器脚本
 --| test
 ----| template.test.sh __ template.sh的测试脚本
+----| template-scale.test.sh __ template-scale.sh的测试脚本
 ```
---
+++ test/template-scale.test.sh
@@ -0,0 +1,19 @@
+# ./template-scale.sh \
+# test-redis \ 
+# 1 \
+# 1 \
+# /home/test-redis \
+# 180.76.155.25 \
+# 200 \
+# 300 \
+# 8080 \
+# 81 \
+# test-redis \
+# test-redis \
+# always \
+# ubuntu:16.04 \
+# https://github.com/EickeOe/nodejs-helloworld \
+# master \
+# src/index.js
+
+./template-scale.sh test-redis 1 1 /home/test-redis 180.76.155.25 200 300 8080 81 test-redis test-redis always ubuntu:16.04 https://github.com/EickeOe/nodejs-helloworld master src/index.js
